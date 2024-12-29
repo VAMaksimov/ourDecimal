d16abc9196ee5f621db2cfb4d098a31ff4622e8b
Chopped Kanisha <choppedk@ge-l5.kzn.21-school.ru>
2024-12-29 13:55:27 +0300

    funcs

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -12,20 +12,21 @@
 #define NULL ((void *)0)
 #endif
 
-#define ROW_NUMBER 4
-#define COLUMN_NUMBER 32
-#define ADDITIONAL_INT_BIT 95
-#define MINUS_BIT_INDEX 127
+typedef struct {
+  int bits[4];
+} s21_decimal;
 
-#define __INT_MIN__ -2147483648
+#define ROW_NUMBER sizeof(s21_decimal) / sizeof(int)
+#define COLUMN_NUMBER sizeof(int) * 8
+#define ADDITIONAL_INT_BIT (3 * COLUMN_NUMBER - 1)
+#define MINUS_BIT_INDEX (ROW_NUMBER * COLUMN_NUMBER - 1)
+
+// #define __INT_MIN__ -2147483648
+#define __INT_MIN__ 0x80000000
 
 #define abs(x) ((x) < 0 ? -(x) : (x))
 #define getRow(x) (x / COLUMN_NUMBER) % ROW_NUMBER
 #define getColumn(x) (x % COLUMN_NUMBER)
-
-typedef struct {
-  int bits[4];
-} s21_decimal;
 
 void nullOutDecimal(s21_decimal *dst);
 void negateDecimal(s21_decimal *dst);
