0f75ef9f4fe08b76fc14593c1f3d571841a374e7
Areo Maire <areomair@ge-j5.kzn.21-school.ru>
2025-01-13 14:07:31 +0300

    na4alo pravok

--- src/components/arithmetics.c
+++ src/components/arithmetics.c
@@ -1,5 +1,50 @@
 #include "../s21_decimal.h"
 
+// int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+//   if (result == NULL || !isCorrectDecimal(&value_1) ||
+//       !isCorrectDecimal(&value_2))
+//     return NUMBER_TOO_SMALL;
+//   if (isDecimalZero(value_2)) return DIVISION_BY_ZERO;
+
+//   int errorType = ADD_OK;
+//   resetDecimal(result);
+
+//   alignScale(&value_1, &value_2, &errorType);
+//   s21_decimal remainder = value_1;
+
+//   if (errorType == ADD_OK) {
+//     int scale = -1;
+//     do {
+//       multiplyBy10(result, &errorType);
+//       printDecimal(*result);
+//       while (isIntPartBiggerOrEqual(remainder, value_2) &&
+//              errorType == ADD_OK) {
+//         s21_decimal spare_value = value_2;
+//         int difference = determineTheSizeDifference(remainder, value_2);
+//         shift_left(&spare_value, difference, &errorType);
+//         subtraction(remainder, spare_value, &remainder, &errorType);
+//         nullOutDecimal(&spare_value);
+//         setBit(&spare_value, 0);
+//         shift_left(&spare_value, difference, &errorType);
+//         addition(*result, spare_value, result, &errorType);
+//         printDecimal(*result);
+//       }
+//       while (!isIntPartBiggerOrEqual(remainder, value_2) &&
+//              errorType == ADD_OK) {
+//         multiplyBy10(&remainder, &errorType);
+//         scale++;
+//       }
+//     } while (!isDecimalZero(remainder) && errorType == ADD_OK);
+
+//     if (isSetBit(value_1, MINUS_BIT_INDEX) !=
+//         isSetBit(value_2, MINUS_BIT_INDEX)) {
+//       setBit(result, MINUS_BIT_INDEX);
+//     }
+//     setScale(result, scale);
+//   }
+//   return errorType;
+// }
+
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   if (result == NULL || !isCorrectDecimal(&value_1) ||
       !isCorrectDecimal(&value_2))
@@ -8,70 +53,31 @@ int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 
   int errorType = ADD_OK;
   resetDecimal(result);
-
-  alignScale(&value_1, &value_2, &errorType);
-  s21_decimal remainder = value_1;
-
-  if (errorType == ADD_OK) {
-    int scale = -1;
-    do {
-      multiplyBy10(result, &errorType);
-      printDecimal(*result);
-      while (isIntPartBiggerOrEqual(remainder, value_2) &&
-             errorType == ADD_OK) {
-        s21_decimal spare_value = value_2;
-        int difference = determineTheSizeDifference(remainder, value_2);
-        shift_left(&spare_value, difference, &errorType);
-        subtraction(remainder, spare_value, &remainder, &errorType);
-        nullOutDecimal(&spare_value);
-        setBit(&spare_value, 0);
-        shift_left(&spare_value, difference, &errorType);
-        addition(*result, spare_value, result, &errorType);
-        printDecimal(*result);
-      }
-      while (!isIntPartBiggerOrEqual(remainder, value_2) &&
-             errorType == ADD_OK) {
-        multiplyBy10(&remainder, &errorType);
-        scale++;
-      }
-    } while (!isDecimalZero(remainder) && errorType == ADD_OK);
-
-    if (isSetBit(value_1, MINUS_BIT_INDEX) !=
-        isSetBit(value_2, MINUS_BIT_INDEX)) {
-      setBit(result, MINUS_BIT_INDEX);
-    }
-    setScale(result, scale);
-  }
-  return errorType;
-}
-
-int s21_div_another(s21_decimal value_1, s21_decimal value_2,
-                    s21_decimal *result) {
-  if (result == NULL || !isCorrectDecimal(&value_1) ||
-      !isCorrectDecimal(&value_2))
-    return NUMBER_TOO_SMALL;
-  if (isDecimalZero(value_2)) return DIVISION_BY_ZERO;
-
-  int errorType = ADD_OK;
-  resetDecimal(result);
-
+  int scale = getScale(value_1) - getScale(value_2);
   alignScale(&value_1, &value_2, &errorType);
   if (errorType != ADD_OK) return errorType;
 
-  s21_decimal remainder = value_1, quotient = {0}, storage = {0};
-  int n = 0;
+  s21_decimal quotient = value_1, remainder = {0};
+  int n = -1;
   for (int i = VALUE_PART_SIZE - 1; i >= 0 && !isSetBit(value_1, i); i--) {
     n = i;
   }
+  n = n + 1;
   for (int i = VALUE_PART_SIZE - 1; i >= 0 && errorType == ADD_OK && n; i--) {
+    s21_decimal temp = remainder;
     shift_left(&remainder, 1, &errorType);
-    shift_left(&storage, 1, &errorType);
+    if (isSetBit(quotient, n - 1)) {
+      setBit(&remainder, 0);
+    }
+    shift_left(&quotient, 1, &errorType);
 
-    subtraction(storage, value_2, &storage, &errorType);
+    subtraction(remainder, value_2, &remainder, &errorType);  // A = A - M
 
-    s21_decimal temp = storage;
-    if (isSetBit(storage, MINUS_BIT_INDEX)) setBit(&remainder, 0);
-    else addition(storage, value_2, &storage, &errorType);
+    if (!isSetBit(remainder, VALUE_PART_SIZE - 1))
+      setBit(&quotient, 0);  // поставить 1 у Q если наибольшая цифра у А не 1
+    else
+      remainder = temp;  // вернуть А, если наибольшая цифра у А 1, Q = 0 -
+                         // последняя цифра
     n--;
   }
 
@@ -79,10 +85,7 @@ int s21_div_another(s21_decimal value_1, s21_decimal value_2,
   if (isSetBit(value_1, MINUS_BIT_INDEX) != isSetBit(value_2, MINUS_BIT_INDEX))
     setBit(&quotient, MINUS_BIT_INDEX);
 
-  // Устанавливаем масштаб результата
-  setScale(&quotient, );
-
-  // Копируем результат
+  setScale(&quotient, scale);
   *result = quotient;
 
   return errorType;
